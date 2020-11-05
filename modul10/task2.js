4e5862c5e5438ac1b3d6861c4939380301875589
Walter Skerf <walter.skerf@gmail.com>
2020-11-05 12:41:39 +0300

    modul10 task1,2,3,4,

---
+++ modul10/task2.js
@@ -0,0 +1,17 @@
+let x = 7 
+switch (typeof x){
+case "number":
+    console.log('x это число');
+    break;
+    case "string":
+        console.log('х это строка');
+break;
+case "boolean":
+    console.log('х это логический тип') ;
+    break;
+    default:
+  
+    console.log('тип х не определён');
+
+}
+
